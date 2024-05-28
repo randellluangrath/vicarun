571438c66e1ce9f87af8d259f9b2b3ced8763e3d
Randell Luangrath <rluangrath@rezultgroup.com>
2024-05-28 16:06:34 -0700

    remove fetch for initial vercel deploy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,25 @@
 // This function fetches data from your API or database
 import DashboardPage from "@/components/client/dashboardPage";
 import {Session} from "@/lib/models/session";
+import {getMockSessions} from "@/lib/utils/firestore";
 
 async function fetchSessions(): Promise<Session[]> {
-    const response = await fetch('http://localhost:3000/api/sessions');
-    if (!response.ok) {
-        throw new Error('Failed to fetch sessions');
-    }
-    const data = await response.json();
-    return data.map((session: any) => (
-        {
-            id: session.id,
-            userId: session.userId,
-            title: session.title,
-            description: session.description,
-            blobUrl: session.blobUrl,
-            createdAt: new Date(session.createdAt),
-        }));
+    // const response = await fetch('http://localhost:3000/api/sessions');
+    // if (!response.ok) {
+    //     throw new Error('Failed to fetch sessions');
+    // }
+    // const data = await response.json();
+    // return data.map((session: any) => (
+    //     {
+    //         id: session.id,
+    //         userId: session.userId,
+    //         title: session.title,
+    //         description: session.description,
+    //         blobUrl: session.blobUrl,
+    //         createdAt: new Date(session.createdAt),
+    //     }));
+
+    return getMockSessions();
 }
 
 export default async function Page() {
