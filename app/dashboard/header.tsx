5e63a6a89b1a991d9477fce8bc61880b756651e2
Randell Luangrath <rluangrath@rezultgroup.com>
2024-05-29 14:31:24 -0700

    adjust header

--- app/dashboard/header.tsx
+++ app/dashboard/header.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header className="w-full shadow-md py-2 px-6 flex items-center fixed top-0 bg-white z-50">
+        <header className="w-full shadow-md px-6 flex items-center fixed top-0 bg-white z-50">
             <h1 className="text-2xl font-bold">vicarun</h1>
-            <Image src="/logo.png" alt="Vicarun Logo" width={50} height={50} />
+            <Image className="mt-1" src="/logo.png" alt="Vicarun Logo" width={50} height={50} />
         </header>
     );
 };
